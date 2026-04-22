9fe978ddaacd61699fbef3da5cd6a3937523e181
Floaty <90103866+FloatingStory@users.noreply.github.com>
2026-04-22 12:47:07 -0400

    add eval zipped

--- chelsea_checks/extract_training_args_bin_dpotrlqlora.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-#load the training_args.bin file found in the dpo model using dpotrainer from trl to extract all hyperparameters used
-training_args = torch.load("training_args.bin")
-
-print(training_args)
